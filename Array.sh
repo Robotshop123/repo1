ba92793e82061e4407e0143bdf2d43581dccc1e3
Robotshop123 <appikatlaambica@gmail.com>
2023-12-17 22:17:52 +0530

    Exmp of array

---
+++ Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+fruits=("Apple" "banana" "mango")
+
+echo "first fruit : ${fruits[0]}"
+echo "second fruit : ${fruits[1]}"
+echo "third fruit : ${fruits[2]}"
+echo "all fruits : ${fruits[@]}"
